94c9af93a6393d6614adad60de773aed247a9739
Hoang Hai <hoanggvanhaii0705>
2024-07-17 21:36:57 +0700

    init project

--- bai1.js
+++ bai1.js
@@ -3,29 +3,24 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-function isPrime(a) {
-    if (a < 2) return false;
-    for (let i = 2; i <= Math.sqrt(a); i++) {
-        if (a % i == 0) return false;
-    }
-    return true;
-}
-for (let i = 0; i < n; i++) {
 
+async function recomment(input) {
+    return new Promise(resolve => rl.question(input, resolve));
 }
-function main() {
+async function main() {
     let a = [];
-    let n;
-    rl.question('nhap n: ', function (cin) {
-        n = cin;
-        for (let i = 0; i < n; i++) {
-            let x;
-            rl.question('nhap x: ', function (scandf) {
-                x = scandf;
-                a.push(x);
-            })
-        }
-        console.log('mnag a: ', a);
-    })
+    let n = parseInt(await recomment('nhap n: '));
+    let b = parseFloat(await recomment('nhap b: '));
+    let c = parseInt(await recomment('nhap c; '));
+    console.log(n, b, c);
+    console.log('\n');
+    for (let i = 0; i < n; i++) {
+        let x = parseInt(await recomment('nhap x: '));
+        a.push(x);
+    }
+
+    console.log('mang a: ', a);
+    rl.close();
 }
+
 main();
