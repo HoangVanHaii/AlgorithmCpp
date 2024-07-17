0fc8ff1dcb9b50c4e664504dce9781095e736541
Hoang Hai <hoanggvanhaii0705>
2024-07-17 22:38:01 +0700

    init project

--- bai.js
+++ bai.js
@@ -7,17 +7,19 @@ const rl = readline.createInterface({
 async function cin(input) {
     return new Promise(resolve => rl.question(input, resolve));
 }
+function isPri(a) {
+    return false;
+}
 
 async function main() {
     let a = parseInt(await cin('nhap gia tri cua a; '));
     console.log(a);
     let b = await cin('nhap b: ');
     console.log(b);
+    // async function abb() {
+    //     let a = await cin('nhap a: ');
+    //     console.log('gia tri phu; ', a);
+    //     rl.close();
+    // }
 }
-async function abb() {
-    let a = await cin('nhap a: ');
-    console.log('gia tri phu; ', a);
-    rl.close();
-}
-abb();
 main();
